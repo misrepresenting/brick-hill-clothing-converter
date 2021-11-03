dbbfd9b832523cea0abee1ecc4ba8225fb8073cb
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-11-03 19:13:39 -0400

    making website ig

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { convertTemplate } = require('./modules/utils.js');
 const sharp = require('sharp');
 const { appendFileSync } = require('fs');
 
-(async () => {
-    const buffer = await sharp('./ta.png').toBuffer();
-    const template = await convertTemplate(buffer);
+const express = require('express');
+const app = express();
 
-    appendFileSync('./lol.png', template);
-})();
+app.listen(80); // http port lol
+
+app.get('/convert', (req, res) => {
+    console.log('A');
+    res.json({ a: 1 });
+});
