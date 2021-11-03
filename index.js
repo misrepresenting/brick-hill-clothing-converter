f391719f71c33a2be48c500bca626db7e18f9297
misrepresenting <64040894+misrepresenting@users.noreply.github.com>
2021-11-03 18:03:34 -0400

    functionallity ez

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const {convertTemplate} = require('./modules/utils.js');
+const { convertTemplate } = require('./modules/utils.js');
 const sharp = require('sharp');
-const {appendFileSync} = require('fs');
+const { appendFileSync } = require('fs');
 
-
-sharp('./ta.png')
-    .toBuffer()
-    .then((buffer) => convertTemplate(buffer))
-    .then((template) => appendFileSync('./lol.png', template));
-
-/*
 (async () => {
     const buffer = await sharp('./ta.png').toBuffer();
     const template = await convertTemplate(buffer);
 
-
-    appendFileSync('./lol.png', template)
-})()
-*/
+    appendFileSync('./lol.png', template);
+})();
